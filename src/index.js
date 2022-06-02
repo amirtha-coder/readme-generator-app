aee70dd9441834e652cc568faf10fffe8c46072d
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-02 13:38:05 +0100

    starter code index

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@
 // https://opensource.org/licenses/MIT
 
 console.log("hello from Node JS");
+
+// import inquirer
+const inquirer = require("inquirer");
+
+// declare questions
+const questions = () => {};
+
+// prompt questions
+const init = (async) => {};
+init();
